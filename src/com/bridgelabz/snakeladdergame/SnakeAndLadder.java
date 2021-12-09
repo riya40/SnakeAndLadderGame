c5a04532306cf052c28d5c3ad1facd49bea61674
riya40 <priyanunna20@gmail.com>
2021-12-09 22:36:09 +0530

    ADDED:Checking For The Options

--- src/com/bridgelabz/snakeladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladdergame/SnakeAndLadder.java
@@ -5,6 +5,7 @@ public class SnakeAndLadder {
         int p = 0,pos = 0;
         int c = (int) Math.floor(Math.random() * 10) % 2;
         System.out.println("not start yet:"+c);
+        int p1=(int) Math.floor(Math.random() * 10) % 5+1;
 
         if(c == 0) {		//player start at 0 position
             System.out.println("we start it now");
@@ -14,6 +15,31 @@ public class SnakeAndLadder {
                 System.out.println("dice rolled:"+p);
                 pos += p;
                 System.out.println("current position:"+pos );
+
+                //for the ladder and snake positions
+                int sw=(int) Math.floor(Math.random() * 10) % 3+1;
+                System.out.println("benifits:"+sw);
+
+                switch(sw) {
+
+                    case 1:
+                        System.out.println("no play");
+                        break;
+                    case 2:
+                        System.out.println("the ladder appeared"+p1);
+                        p+=p1;
+                        System.out.println("the ladder appeared:"+p);
+                        break;
+                    case 3:
+                        System.out.println("the snake appeared"+p1);
+                        p-=p1;
+                        System.out.println("the snake appeared"+p);
+                        break;
+                    default :
+                        System.out.println("no change");
+                        break;
+                }
+
             }
 
         }
