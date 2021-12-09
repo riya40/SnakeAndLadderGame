8fb66ae10dceae80439c06421e0a0315c9659334
riya40 <priyanunna20@gmail.com>
2021-12-09 22:16:59 +0530

    ADDED:Roll the Dice To get The Number

--- src/com/bridgelabz/snakeladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladdergame/SnakeAndLadder.java
@@ -2,16 +2,17 @@ package com.bridgelabz.snakeladdergame;
 
 public class SnakeAndLadder {
     public static void main(String args[]) {
-        int p=0,pos=0;
-        int c=(int) Math.floor(Math.random() * 10) % 2;
+        int p = 0,pos = 0;
+        int c = (int) Math.floor(Math.random() * 10) % 2;
         System.out.println("not start yet:"+c);
 
-        if(c==0) {		//player start at 0 position
+        if(c == 0) {		//player start at 0 position
             System.out.println("we start it now");
+            //rolling the dice to get the number and showing the position
             for(int i=0;i<6;i++) {
-                p=(int) Math.floor(Math.random() * 10) % 5+1;//dice roll to get number
+                p = (int) Math.floor(Math.random() * 10) % 5+1;//dice roll to get number using random function
                 System.out.println("dice rolled:"+p);
-                pos+=p;
+                pos += p;
                 System.out.println("current position:"+pos );
             }
 
